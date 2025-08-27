5c783a735edc71de912da457e0d552487c8fc7b4
amos-babu <amos.babu@yahoo.com>
2025-08-27 16:46:19 +0300

    'get request'

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,9 +1,7 @@
 import Card from "@/components/cards";
-import Loading from "@/components/loading";
-import { PostsProps } from "@/lib/types";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { useState } from "react";
+import fetchPaginatedPosts from "@/lib/posts";
+import { FilesProp } from "@/lib/types";
+import { useEffect, useState } from "react";
 import { FlatList, View } from "react-native";
 import {
   SafeAreaView,
@@ -14,41 +12,52 @@ const API_URL = process.env.EXPO_PUBLIC_API_URL;
 // retrieve_files;
 
 export default function Index() {
-  const [data2, setData2] = useState({});
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["posts"],
-    queryFn: async () => {
-      const response = await axios.get<PostsProps>(`${API_URL}/retrieve_files`);
-      setData2(response.data);
-      return response.data.data.files;
-    },
-  });
+  const [posts, setPosts] = useState<FilesProp[]>([]);
+  // const { data, isLoading, isError, error } = useQuery({
+  //   queryKey: ["posts"],
+  //   queryFn: async () => {
+  //     const response = await axios.get<PostsProps>(`${API_URL}/retrieve_files`);
+  //     setData2(response.data);
+  //     return response.data.data.files;
+  //   },
+  // });
 
-  console.log(data2);
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const result = await fetchPaginatedPosts();
+        setPosts(result.data.files);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchPost();
+  }, []);
 
   const insets = useSafeAreaInsets();
 
   return (
     <SafeAreaView className="bg-white h-full">
       <View className="px-5">
-        {isLoading ? (
+        {/* {isLoading ? (
           <Loading />
-        ) : (
-          <FlatList
-            data={data}
-            numColumns={2}
-            columnWrapperStyle={{ justifyContent: "space-between" }}
-            contentContainerStyle={{
-              paddingBottom: insets.bottom + 70,
-            }}
-            renderItem={({ item }) => (
-              <View className="flex-1 items-center my-2">
-                <Card posts={item} />
-              </View>
-            )}
-            keyExtractor={(item) => item.id.toString()}
-          />
-        )}
+        ) : ( */}
+        <FlatList
+          data={posts}
+          numColumns={2}
+          columnWrapperStyle={{ justifyContent: "space-between" }}
+          contentContainerStyle={{
+            paddingBottom: insets.bottom + 70,
+          }}
+          renderItem={({ item }) => (
+            <View className="flex-1 items-center my-2">
+              <Card posts={item} />
+            </View>
+          )}
+          keyExtractor={(item) => item.id.toString()}
+        />
+        {/* )} */}
       </View>
     </SafeAreaView>
   );
--- lib/posts.ts
+++ lib/posts.ts
@@ -1,12 +1,11 @@
-import axios from "axios";
-import { PostsProps } from "./types";
-
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
-const fetchPaginatedPosts = async ({ pageParams }: { pageParams: string }) => {
+const fetchPaginatedPosts = async () => {
   try {
-    const response = await axios.get<PostsProps>(pageParams);
-    return response.data;
+    const response = await fetch(`http://127.0.0.1:8000/api/retrieve_files`);
+    const results = await response.json();
+    console.log(results);
+    return results;
   } catch (error) {
     console.log("Error fetching post: ", error);
     throw error;
